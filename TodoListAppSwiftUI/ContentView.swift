3134c8803338ff1d132fe556e7f54a2109de708a
vanxuat <xuat0986782902@gmail.com>
2025-03-01 11:52:04 +0700

    init project

---
+++ TodoListAppSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TodoListAppSwiftUI
+//
+//  Created by van xuat Tran on 1/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TodoListAppSwiftUI/TodoListAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListAppSwiftUIApp.swift
+//  TodoListAppSwiftUI
+//
+//  Created by van xuat Tran on 1/3/25.
+//
+
+import SwiftUI
+
+@main
+struct TodoListAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
